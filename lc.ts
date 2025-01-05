2c95a693f63f3e2a030732c1c7646efac8c1fb1f
ted <kerdek7@gmail.com>
2025-01-05 17:30:53 -0500

    added some comments

--- lc.ts
+++ lc.ts
@@ -30,6 +30,7 @@ export type Graph = Term | Sav
 export type Kind = Graph['kind']
 type Rest<i, Graph> = Graph extends { kind: i } & infer R ? R : never
 type Sorts = { [i in Kind]: Rest<i, Graph> }
+type Visit = <K extends Kind>(o: { [i in K]: (e: Sorts[i]) => Branch }) => <I extends K>(e: Sorts[I]) => Process
 
 // Types of the functions exported
 export type Read = (src: Tokenizer) => Term
@@ -37,10 +38,10 @@ export type Print = (e: Graph) => string
 export type Bubble = (e: Sav) => Term
 export type Evaluate = (e: Graph) => Normal
 
+// Delete all properties on `e` and and copy all properties from `x`.
+// Must be self-assignment safe.
 type Assign = <K extends { [i: string]: any }>(e: { [i: string]: any }, x: K) => K
-type VisitBranch = <K extends Kind>(o: { [i in K]: (e: Sorts[i]) => Branch }) => <I extends K>(e: Sorts[I]) => Process
-
-export const assign: Assign = (e, x) => {
+const assign: Assign = (e, x) => {
   if (e === x) {
     return e }
   for (const k in e) {
@@ -49,10 +50,13 @@ export const assign: Assign = (e, x) => {
     e[k] = x[k] }
   return e as any }
 
-export const visit_branch: VisitBranch = o => e => (f => () => f(e))(o[e.kind as keyof typeof o])
+// pattern for branching by table lookup
+export const visit: Visit = o => e => (f => () => f(e))(o[e.kind as keyof typeof o])
 
+// pattern for left-to-right IIFEs
 const di: <X, Y, F extends (x: X) => Y>(x: X, f: F) => Y = (x, f) => f(x)
 
+// accept a tokenizer and give an AST.
 export const read: Read = tk => homproc((call, ret) => {
 const
 fatal: (msg: string) => never = m => {
@@ -60,21 +64,22 @@ fatal: (msg: string) => never = m => {
 parameters: Process = () =>
   tk.take("dot") ? jmp(expression) :
   di(tk.take("identifier"), i =>
-  i ? call(parameters, dx => ret({ kind: "abs", param: i[1], body: dx })) :
-  fatal(`Expected \`.\`.`)),
+  !i ? fatal(`Expected \`.\`.`) :
+  call(parameters, dx =>
+  ret({ kind: "abs", param: i[1], body: dx }))),
 try_primary: () => Process | null = () =>
   tk.take("rsolidus") ? parameters :
-  tk.take("lparen") ?
-    () =>
-      call(expression, x =>
-      tk.take("rparen") ? ret(x) :
-      fatal(`Expected \`)\`.`)) :
+  tk.take("lparen") ? () =>
+    call(expression, x =>
+    tk.take("rparen") ? ret(x) :
+    fatal(`Expected \`)\`.`)) :
   di(tk.take("identifier"), r =>
-  r ? () => ret({ kind: "var", name: r[1] }) : null),
+  r ? () => ret({ kind: "var", name: r[1] }) :
+  null),
 primary: Process = () =>
-di(try_primary(), up =>
-up === null ? fatal("Expected a term.") :
-jmp(up)),
+  di(try_primary(), up =>
+  up === null ? fatal("Expected a term.") :
+  jmp(up)),
 juxt_rhs: (lhs: Term) => Branch = lhs =>
   di(try_primary(), up =>
   up === null ? ret(lhs) :
@@ -88,13 +93,16 @@ all: Process = () =>
   ret(e))
 return all })
 
+// accept an AST and give a string.
+// ignore the definition of save nodes.
+// just print the name of share nodes.
 export const print: Print = e => homproc((call, ret) => {
 const
 p: (q: boolean, t: string) => string = (q, t) => q ?`(${t})` : t,
 l: (e: Graph) => Process = e => () =>
   e.kind === "abs" ? call(l(e.body), dx => ret(`${e.param} ${dx}`)) :
   call(s(false, true)(e), dx => ret(`.${dx}`)),
-s: (pr: boolean, rm: boolean) => (e: Graph) => Process = (pr, rm) => visit_branch({
+s: (pr: boolean, rm: boolean) => (e: Graph) => Process = (pr, rm) => visit({
   app: ({ lhs, rhs }) =>
     call(s(false, false)(lhs), dx =>
     call(s(true, rm || pr)(rhs), dy =>
@@ -107,20 +115,41 @@ s: (pr: boolean, rm: boolean) => (e: Graph) => Process = (pr, rm) => visit_branc
   shr: ({ name }) => ret(name) })
 return s(false, true)(e) })
 
+// accept a save node and return a non-save node by
+// bubbling the definition over the body
 export const bubble: Bubble = e => homproc((call, ret) => {
-const s: (e: Sav) => Process = e => () => call(visit_branch({
+const sp = visit({
   sav: y => call(s(y), () => jmp(s(e))),
-  app: ({ lhs, rhs }) => ret({ kind: "app", lhs: { kind: "sav", definition: e.definition, body: lhs }, rhs: { kind: "sav", definition: e.definition, body: rhs } }),
-  abs: y => y.param === e.definition.name ? ret(y) : ret({ kind: "abs", param: y.param, body: { kind: "sav", definition: e.definition, body: y.body } }),
+  app: ({ lhs, rhs }) =>
+    ret({ kind: "app",
+      lhs: { kind: "sav", definition: e.definition, body: lhs },
+      rhs: { kind: "sav", definition: e.definition, body: rhs } }),
+  abs: y =>
+    y.param === e.definition.name ? ret(y) :
+    ret({ kind: "abs", param: y.param,
+      body: { kind: "sav", definition: e.definition, body: y.body } }),
   var: y => ret(y.name === e.definition.name ? e.definition : y),
-  shr: ret })(e.body), de => ret(assign(e, de)))
+  shr: ret })
+const s: (e: Sav) => Process = e => () =>
+  call(sp(e.body), de =>
+  ret(assign(e, de)))
 return s(e) })
 
+// evaluate an AST and return another AST
+// mutates share nodes in the input
+// for save nodes, bubble and retry.
+// for application nodes, evaluate the lhs and generate a save node.
+// for share nodes, evalute the pointed node
 export const evaluate: Evaluate = e => homproc((call, ret) => {
-const s: (e: Graph) => Process = visit_branch({
+const s: (e: Graph) => Process = visit({
   sav: e => jmp(s(bubble(e))),
-  app: ({ lhs, rhs }) => call(s(lhs), dx => jmp(s({ kind: "sav", definition: { kind: "shr", name: dx.param, ptr: rhs }, body: dx.body }))),
-  shr: e => call(s(e.ptr), dx => (e.ptr = dx, ret(dx))),
+  app: ({ lhs, rhs }) =>
+    call(s(lhs), dx =>
+    jmp(s({ kind: "sav", definition: { kind: "shr", name: dx.param, ptr: rhs }, body: dx.body }))),
+  shr: e =>
+    call(s(e.ptr), dx => (
+    e.ptr = dx,
+    ret(dx))),
   var: ({ name }) => { throw new Error(`Undefined reference to \`${name}\`.`)},
   abs: ret })
 return s(e) })

7c190aab91eccab0e16c14deb2b5f80bb39d88e3
ted <kerdek7@gmail.com>
2025-01-06 08:38:23 -0500

    fix comments

--- evaluate.ts
+++ evaluate.ts
@@ -2,12 +2,6 @@ import { Term, Normal, visit, Env } from "./graph.js"
 import { Process, homproc, jmp } from "./run.js"
 
 // evaluate an AST and return another AST
-// for save nodes, bubble and retry.
-// for application nodes, evaluate the lhs and generate a save node.
-// for share nodes, evalute the pointed node and assign the pointer.
-// for variable nodes, look it up in the environment,
-// evaluate it and update the pointer.
-// for abstraction nodes, save the environment.
 export const evaluate: (e: Term, env: Env) => Normal = (e, env) => homproc((call, ret) => {
 const s: (e: Term, env: Env) => Process = (e, env) => visit({
   app: ({ lhs, rhs }) =>
